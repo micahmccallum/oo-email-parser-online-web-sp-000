7fc63c265c11ba996a7a5983f556e82c49400b0b
Micah McCallum <micahmccallum@outlook.com>
2020-04-02 19:31:30 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ class EmailAddressParser
 
   def self.parse
     parsed = []
-    parsed = self.class.delete(",").split(" ").uniq
+    parsed = self.class.addresses.delete(",").split(" ").uniq
     parsed
   end
 
